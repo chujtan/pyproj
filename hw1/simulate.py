ee811aa81a193162fc02fe29df6dc4c341449d46
Chu Tan <chujtan@gmail.com>
2013-10-26 08:58:08 -0500

    hw1

--- hw1/simulate.py
+++ hw1/simulate.py
@@ -2,6 +2,10 @@ __author__ = 'ChuTan'
 
 import datetime as dt
 import QSTK.qstkutil.DataAccess as da
+import QSTK.qstkutil.qsdateutil as du
+import QSTK.qstkutil.tsutil as tu
+import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def simulate(startdate, enddate, symbols, weights):
@@ -10,8 +14,27 @@ def simulate(startdate, enddate, symbols, weights):
     sharpe = 0
     cum_ret = 0
 
-    q = da.DataAccess("Yahoo")
-    ts_list = ["AAPL"]
-    symbol_list = [ dt.datetime(2010,6,1) ]
-    df_close = q.get_data(ts_list, symbol_list, "adjclose")
+    q = da.DataAccess(da.DataSource.YAHOO)
+    ldt_timestamps = du.getNYSEdays(startdate, enddate, dt.timedelta(hours=16))
+    ls_keys = [da.DataItem.HIGH, da.DataItem.LOW, da.DataItem.CLOSE, da.DataItem.ADJUSTED_CLOSE]
+    ldf_data = q.get_data(ldt_timestamps, symbols, ls_keys)
+    return dict(zip(ls_keys, ldf_data))
 
+#def plot():
+#    plt.legend([])
+#    plt.ylabel('Close')
+#    plt.xlabel('Date')
+#    plt.plot(ts, prices)
+#    plt.show()
+
+
+def main():
+    sd = dt.datetime(2009, 1, 1)
+    ed = dt.datetime(2009, 12, 31)
+    s = ["GOOG", "GS", "XLF", "XLK", "$SPX", "XOM", "JNJ", "IBM", "AAPL", "GLD"]
+    w = [1.0 / len(s) for i in range(len(s))]
+    return simulate(sd, ed, s, w)
+
+
+if __name__ == "__main__":
+    main()
