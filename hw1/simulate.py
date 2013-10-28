38bbd14db1715621eef1a21df41051ec45173405
Chu Tan <chujtan@gmail.com>
2013-10-28 08:57:25 -0500

    hw1

--- hw1/simulate.py
+++ hw1/simulate.py
@@ -6,46 +6,67 @@ import QSTK.qstkutil.qsdateutil as du
 import QSTK.qstkutil.tsutil as tu
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 
-def simulate(startdate, enddate, symbols, weights):
+def calc_returns(df_price):
+    return df_price / df_price.shift(1)
+
+def calc_vol(df_pret):
+    df_count = df_pret.count()
+    return (((df_pret - (df_pret.sum() / df_count)) ** 2) / df_count) ** 0.5
+
+def simulate(dt_sd, dt_ed, l_sym, l_w):
     vol = 0
     daily_ret = 0
     sharpe = 0
     cum_ret = 0
 
-    df_close = get_data(startdate, enddate, symbols, [da.DataItem.CLOSE])[da.DataItem.CLOSE]
-    df_ret = df_close / df_close.shift(1) - 1
-    df_avg = df_ret.sum() / df_ret.count()
-    df_var = (df_ret - df_avg) ** 2 / df_ret.count()
-    df_vol = df_var ** 0.5
+    df_close = get_data(dt_sd, dt_ed, l_sym, [da.DataItem.CLOSE])[da.DataItem.CLOSE]
+    if not(checkNAs(df_close)): return
+    df_value = sim_value(df_close, l_w)
+    df_pvalue = df_value.sum(axis=1)
+    df_pret = calc_returns(df_pvalue)
+    df_pvol = calc_vol(df_pret)
+    df_pcumret = df_pvalue / df_pvalue[0]
 
+    plot(["PValue"], df_pvalue.index, df_pvalue)
 
+def checkNAs(df_price):
+    for col in df_price.columns:
+        if np.any(df_price[col].isnull()):
+            print "Col " + col + " contains NaN"
+            return False
+    return True
 
+def rejectAllNAs(df_price):
+    drop = []
+    for col in df_price.columns:
+        if np.all(df_price[col].isnull()):
+            drop.append(col)
+    print "Dropping " + str(drop)
+    for col in drop: del df_price[col]
+    return drop
 
-def get_data(dt_sd, dt_ed, l_symbols, l_keys):
+def sim_value(df_close, qtys):
+    return df_close * qtys
+
+def get_data(dt_sd, dt_ed, l_sym, keys):
     q = da.DataAccess(da.DataSource.YAHOO)
-    l_ts = du.getNYSEdays(dt_sd, dt_ed, dt.timedelta(hours=16))
-    l_dfs = q.get_data(l_ts, l_symbols, l_keys)
-    return dict(zip(l_keys, l_dfs))
+    timestamps = du.getNYSEdays(dt_sd, dt_ed, dt.timedelta(hours=16))
+    dfs = q.get_data(timestamps, l_sym, keys)
+    return dict(zip(keys, dfs))
 
-
-#def plot():
-#    plt.legend([])
-#    plt.ylabel('Close')
-#    plt.xlabel('Date')
-#    plt.plot(ts, prices)
-#    plt.show()
-
-
-def main():
-    sd = dt.datetime(2009, 1, 1)
-    ed = dt.datetime(2009, 12, 31)
-    s = ["GOOG", "GS", "XLF", "XLK", "$SPX", "XOM", "JNJ", "IBM", "AAPL", "GLD"]
-    w = [1.0 / len(s) for i in range(len(s))]
-    l_keys = [da.DataItem.HIGH, da.DataItem.LOW, da.DataItem.CLOSE, da.DataItem.ACTUAL_CLOSE]
-    return get_data(sd, ed, s, l_keys)
-
+def plot(legend, ts, s):
+    plt.legend(legend)
+    plt.xlabel('Date')
+    plt.plot(ts, s)
+    plt.show()
 
 if __name__ == "__main__":
-    main()
+    sd = dt.datetime(2009, 1, 1)
+    ed = dt.datetime(2009, 12, 31)
+    s = ["GOOG", "GS", "$SPX", "XOM", "JNJ", "IBM", "AAPL", "GLD"]
+    w = [1.0 / len(s) for i in range(len(s))]
+    df_close = get_data(sd, ed, s, [da.DataItem.CLOSE])[da.DataItem.CLOSE]
+
